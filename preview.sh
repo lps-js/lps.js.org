a40b721f78e19fdb0f72458480dbff464ccdd8a7
Sam Yong <samyonggit@gmail.com>
2018-08-20 12:53:24 +0100

    fix preview building

--- preview.sh
+++ preview.sh
@@ -1,5 +1,8 @@
 #! /bin/sh
 rm -rf build
 git clone git@github.com:mauris/lps.js.org.git build
+cd build
+git checkout master
+cd ..
 node ./node_modules/pug-cli/index.js -o build src
 cp -R src/assets build/assets
