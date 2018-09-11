368cefae62b117537973ea01ef9100463ed06caa
Sam Yong <samyonggit@gmail.com>
2018-09-12 00:49:25 +0100

    update build script

--- preview.sh
+++ preview.sh
@@ -3,6 +3,10 @@ rm -rf build
 git clone git@github.com:mauris/lps.js.org.git build
 cd build
 git checkout master
+git rm -rf .
 cd ..
 node ./node_modules/pug-cli/index.js -o build src
+cp CNAME build/
 cp -R src/assets build/
+cd build
+git add -A
