971b379adc6545128b490f5707b2ab761b95cd01
Sam Yong <samyonggit@gmail.com>
2018-09-18 15:58:28 +0100

    add watch script

---
+++ watch.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+rm -rf build
+git clone git@github.com:mauris/lps.js.org.git build
+cd build
+git checkout master
+git rm -rf .
+cd ..
+cp CNAME build/
+cp -R src/assets build/
+node ./node_modules/pug-cli/index.js -w -o build src
