56927c918577ee6840dca1da4f6cd97a100a7a77
Sam Yong <samyonggit@gmail.com>
2018-09-19 11:15:21 +0100

    update watch script to run static server

--- scripts/watch.sh
+++ scripts/watch.sh
@@ -7,4 +7,5 @@ git rm -rf .
 cd ..
 cp CNAME build/
 cp -R src/assets build/
+cd build && node ../node_modules/static-server/bin/static-server.js &
 node ./node_modules/pug-cli/index.js -w -o build src
